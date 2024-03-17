3d3e671f49469388bd201e0c9db28d9ffb84c4f2
BEN AMOR Aziz <benamoraziz0220@gmail.com>
2024-03-17 18:35:59 +0100

    added avatar

--- src/app/services/avatar.service.ts
+++ src/app/services/avatar.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { ref, Storage, uploadString } from '@angular/fire/storage';
+import { Photo } from '@capacitor/camera';
+import { setDoc } from 'firebase/firestore';
+import { getDownloadURL } from 'firebase/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvatarService {
 
-  constructor() { }
+  constructor(private auth: Auth, private firestore: Firestore, private storage: Storage) { }
+
+  getUserProfile(){
+    const user = this.auth.currentUser;
+    const userDocRef = doc(this.firestore, `users/${user?.uid}`);
+    return docData(userDocRef, {idField: 'id'});
+  }
+
+  async uploadImage(cameraFile: Photo){
+    const user = this.auth.currentUser;
+    const path = `uploads/${user?.uid}/profile.webp`;
+    const storageRef = ref(this.storage, path);
+
+    try {
+      await uploadString(storageRef, cameraFile.base64String+'', 'base64');
+      const imageUrl = await getDownloadURL(storageRef);
+      const userDocRef = doc (this.firestore, `users/${user?.uid}`);
+      await setDoc(userDocRef, {imageUrl});
+      return true;
+    }
+    catch(e){
+      return null;
+    }
+  }
 }
